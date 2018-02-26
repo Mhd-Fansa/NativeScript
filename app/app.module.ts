a549d1afdde2238daa4d325cba5085bdb0d6f9ac
Mhd Fansa <m.fansa92@gmail.com>
2018-02-26 16:04:46 +0300

    Navigation

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
+import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
@@ -23,16 +28,21 @@ import { baseURL } from './shared/baseurl';
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        NativeScriptHttpModule
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        HomeComponent,
+        DishdetailComponent,
+        DrawerComponent
     ],
     providers: [
       {provide: 'BaseURL', useValue: baseURL},
       DishService,
+      LeaderService,
+      PromotionService,
       ProcessHTTPMsgService
     ],
     schemas: [
